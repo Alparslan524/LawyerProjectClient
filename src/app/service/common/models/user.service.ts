0f6a1740d375a49b961675c8f817bd37a911c55a
Alparslan <alparslanaydgn02@gmail.com>
2023-10-31 16:13:57 +0300

    JWT entegrasyonu yapıldı. Artık giriş yapmadan cases dönülmüyor. Giriş yapınca token dönüyor.

--- src/app/service/common/models/user.service.ts
+++ src/app/service/common/models/user.service.ts
@@ -3,13 +3,15 @@ import { HttpClientService } from '../http-client-service.service';
 import { User } from 'src/app/entities/User';
 import { CreateUser } from 'src/app/contracts/User/create-user';
 import { Observable, firstValueFrom } from 'rxjs';
+import { TokenResponse } from 'src/app/contracts/Token/tokenResponse';
+import { AlertifyService, MessageType, Position } from '../alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService, private alertifyService: AlertifyService) { }
 
   async create(user: User) {
     const observable: Observable<CreateUser | User> = this.httpClientService.post<CreateUser | User>({
@@ -19,12 +21,19 @@ export class UserService {
     return await firstValueFrom(observable) as CreateUser;
   }
 
-  async login(userNameOrEmail: string, password: string, callBack?: () => void): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post({
+  async login(userNameOrEmail: string, password: string, callBack?: () => void): Promise<any> {
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "auth",
       action: "login"
     }, { userNameOrEmail, password })
-    await firstValueFrom(observable);
+    const tokenResponse: TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if (tokenResponse) {
+      localStorage.setItem("accessToken", tokenResponse.token.accessToken);
+      this.alertifyService.message("Kullanıcı girişi başarılı", {
+        messageType: MessageType.Success,
+        position: Position.TopRight
+      })
+    }
     callBack();
   }
 
